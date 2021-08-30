d6969057e26b370643ae169c2d3305608545b147
gpranaykumar <65266203+gpranaykumar@users.noreply.github.com>
2021-08-30 23:50:08 +0530

    Delete server directory

--- server/routes/cart.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const cartController = require('../controllers/cartController');
-
-router.get('/cart/:id', cartController.getCartItems);
-router.post('/cart/:id', cartController.addCartItem );
-router.delete('/cart/:userId/:itemId', cartController.deleteCartItem);
-
-module.exports = router;
--- server/routes/order.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const orderController = require('../controllers/orderController');
-
-router.get('/order/:id', orderController.getOrders);
-router.post('/order/:id', orderController.checkout);
-
-module.exports = router;
